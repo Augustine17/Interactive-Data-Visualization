eea9724c78a28688106f7ae395333dfaf2692a0e
Augustine17 <augmen173@gmail.com>
2024-02-03 22:54:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const myChart = document.querySelector("#myChart");
+
+console.log(myChart);
+
+new Chart(myChart, {
+  type: "bar",
+  data: {
+    labels: ["Jan", "Fab", "Mar", "Apr", "May", "June"],
+    datasets: [
+      {
+        label: "Scale",
+        data: [20, 30, 50, 35, 80, 65],
+        backgroundColor: [
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(201, 203, 207, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+          "rgba(255, 205, 86, 0.2)",
+        ],
+        borderColor: [
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+        ],
+
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
+
